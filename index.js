1bb153f0e948d839deeb3312eee16b8be26b678e
MrBarker47 <jebarker7@gmail.com>
2024-12-15 12:31:08 -0500

    Update files

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import express from 'express'
+const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send('Hello, Jamaal Barker');
+})
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:, port");
+})
