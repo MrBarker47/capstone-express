5f582b8de992acf8e7f4fa09123110323a1ff994
MrBarker47 <jebarker7@gmail.com>
2024-12-15 12:39:24 -0500

    Update files

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+//express 
 const express = require("express");
+const mongoose = require("mongoose");
 
+//PORT INFO
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 
 app.use(express.json());
 
+
+
+//mongoose connection
+mongoose.connect("mongodb+srv://MrBarker47:WfhpISelxhFNvZyD@mongopractice.mkbxl.mongodb.net/?retryWrites=true&w=majority&appName=MongoPractice")
+.then(() => {
+    console.log("Connect to database");
+})
+
+
+//HTTP METHODS
 app.get("/", (req, res) => {
     res.send('Hello, Jamaal Barker');
 })
