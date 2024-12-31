1b0dd789c175df815c8a06b9c09a8bf031b8b61f
MrBarker47 <jebarker7@gmail.com>
2024-12-31 18:22:40 -0500

    Update files

--- routes/food_Route.js
+++ routes/food_Route.js
@@ -3,24 +3,58 @@ import { Food } from "../modules/food_Data.js"
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try{
-        const food = await Food.find({});
-        res.status(200).json(food)
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    };
-    
-});
+
 
 router.post('/', async (req, res) => {
     try {
-        const food = await Food.create();
-        res.status(200).json(food);
-    } catch (err) {
-        res.status(400).json({message: err.message});
-    };
+        if(
+            !req.body.name || 
+            !req.body.pricePoint ||
+            !req.body.img
+        ) {
+            return res.status(400).send({
+                message: 'Required fields are missing'
+            })
+        }
+
+        const newFood = {
+            name: req.body.name,
+            pricePoint: req.body.pricePoint,
+            imgage: req.body.imgage
+        };
+
+        const food = await Food.create(newFood);
+
+        return res.status(200).send(food);
+
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
 });
+
+
+
+
+// router.get('/', async (req, res) => {
+//     try{
+//         const food = await Food.find({});
+//         res.status(200).json(food)
+//     } catch (err) {
+//         res.status(500).json({message: err.message});
+//     };
+    
+// });
+
+// router.post('/', async (req, res) => {
+//     try {
+//         const food = await Food.create();
+//         res.status(200).json(food);
+//     } catch (err) {
+//         res.status(400).json({message: err.message});
+//     };
+// });
 
 
 
